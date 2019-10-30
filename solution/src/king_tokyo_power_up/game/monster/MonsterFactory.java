eaabb1464eaa98c4b7af5ae11d256e709c0878be
Alexander Mennborg <alexander.mennborg@gmail.com>
2019-10-30 14:42:23 +0100

    Update MonsterFactory.java

--- solution/src/king_tokyo_power_up/game/monster/MonsterFactory.java
+++ solution/src/king_tokyo_power_up/game/monster/MonsterFactory.java
@@ -1,5 +1,7 @@
 package king_tokyo_power_up.game.monster;
 
+import king_tokyo_power_up.game.card.DeckFactory;
+
 import java.util.ArrayList;
 
 /**
@@ -19,21 +21,27 @@ public class MonsterFactory {
     public static ArrayList<Monster> createMonsters() {
         ArrayList<Monster> monsters = new ArrayList<>();
         Monster alienoid = new Monster("Alienoid", "Alien");
+        alienoid.setEvolutionDeck(DeckFactory.createEvolutionDeck("Alienoid"));
         monsters.add(alienoid);
 
         Monster gigazaur = new Monster("Gigazaur", "Mutant");
+        alienoid.setEvolutionDeck(DeckFactory.createEvolutionDeck("Gigazaur"));
         monsters.add(gigazaur);
 
         Monster kong = new Monster("Kong", "Mutant");
+        alienoid.setEvolutionDeck(DeckFactory.createEvolutionDeck("Kong"));
         monsters.add(kong);
 
         Monster pumpkinJack = new Monster("Pumpkin Jack", "Halloween");
+        alienoid.setEvolutionDeck(DeckFactory.createEvolutionDeck("Pumpkin Jack"));
         monsters.add(pumpkinJack);
 
         Monster rob = new Monster("Rob", "Robot");
+        alienoid.setEvolutionDeck(DeckFactory.createEvolutionDeck("Rob"));
         monsters.add(rob);
 
         Monster cthulhu = new Monster("Cthulhu", "Alien");
+        alienoid.setEvolutionDeck(DeckFactory.createEvolutionDeck("Cthulhu"));
         monsters.add(cthulhu);
 
         return monsters;
